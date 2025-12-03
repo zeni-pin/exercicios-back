5193f14a9ce36c7fb58bff92441c71b4db18ae97
Zeni <zeni@pinaculodigital.com.br>
2025-12-03 13:30:54 -0300

    update service and dto

--- src/core/modules/public/posts/dto/list-posts.dto.ts
+++ src/core/modules/public/posts/dto/list-posts.dto.ts
@@ -1,3 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import { PaginationDto } from 'src/core/types/dto/pagination.dto';
 
-export class ListPostsDto extends PaginationDto {}
+class QueryData {
+  @ApiProperty({ example: 'Search', required: false })
+  search?: string;
+}
+
+class PostData {
+  @ApiProperty({ example: 'clxxx123456789', required: false })
+  id: string;
+
+  @ApiProperty({ example: 'Título', required: false })
+  title: string;
+
+  @ApiProperty({ example: 'Conteúdo', required: false })
+  content: string;
+
+  @ApiProperty({ example: '2024-01-01T00:00:00.000Z', required: false })
+  createdAt: Date;
+
+  @ApiProperty({ example: 'Autor' })
+  author: string;
+}
+
+export class ListPostsDto extends PaginationDto {
+  @ApiProperty({ example: 'Search', required: false })
+  @IsOptional()
+  @IsString()
+  search?: string;
+}
+
+export class ListResponse {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+  data: Array<PostData>;
+
+  query: QueryData;
+}
--- src/core/modules/public/posts/posts.service.ts
+++ src/core/modules/public/posts/posts.service.ts
@@ -1,12 +1,69 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/integrations/persistence/database/prisma/prisma.service';
-import { ListPostsDto } from './dto/list-posts.dto';
+import { ListPostsDto, ListResponse } from './dto/list-posts.dto';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async listPostsByUsername(params: { username: string; filters: ListPostsDto }) {
-    return await this.prisma.post.findMany();
+  async listPostsByUsername(params: {
+    username: string;
+    filters: ListPostsDto;
+  }): Promise<ListResponse> {
+    const { filters, username } = params;
+    const { limit, page, search } = filters;
+
+    let where: Prisma.PostWhereInput = {
+      author: {
+        username,
+      },
+    };
+
+    if (search) {
+      where.title = {
+        contains: search,
+        mode: 'insensitive',
+      };
+    }
+
+    const [data, total] = await Promise.all([
+      this.prisma.post.findMany({
+        where,
+        orderBy: { createdAt: 'desc' },
+        skip: (page - 1) * limit,
+        take: limit,
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          createdAt: true,
+        },
+      }),
+      this.prisma.post.count({ where }),
+    ]);
+
+    const postData = data.map((post) => {
+      const { content, createdAt, id, title } = post;
+
+      return {
+        id,
+        title,
+        content,
+        createdAt,
+        author: username,
+      };
+    });
+
+    return {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+      data: postData,
+      query: {
+        search,
+      },
+    };
   }
 }
